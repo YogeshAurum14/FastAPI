6c79458b448455566a8e13cb5ad46f1531e84c5a
YogeshAurum14 <90750856+YogeshAurum14@users.noreply.github.com>
2021-11-11 13:08:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import pymongo
+from pymongo import MongoClient
+import json
+from pprint import pprint
+
+### DB connectivity
+cluster = MongoClient("mongodb+srv://AurumUser:latestPlacid@cluster0.icdds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["igr"]
+collection = db["igr"]
+
+
+
+# create instance of fastapi
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return{"message": "Hello Bitches and mofos"}
+
+figu={}
+@app.get("/search/{location} {reraid}")
+async def search(location:str, reraid:str):
+    data = collection.find()
+    c=0
+    try:
+        for i in data:
+            for k,v in i.items():
+                descr=i['translated description']
+                rera=i["RERA Code"]
+                if ((location.lower() in descr.lower()) and rera==reraid):
+                    c += 1
+                    pprint(i)
+    except:
+        pass
+    print(c)
+    return {"status": "SUCCESS"}
